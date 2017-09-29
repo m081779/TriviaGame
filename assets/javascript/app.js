cbf22488e8615152db8b06342e6ac58a486307da
m081779 <marcoprincipio@hotmail.com>
2017-09-29 02:07:41 -0400

    Working prototype, no known bugs.  Still needs work cosmetically

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,7 +14,8 @@ var game = {
 	reset: false,
 	timeRemaining: 0,
 	score: 0,
-	scoreArr: [],
+	scoreArr: [500,400,300,200,100],
+	nameArr: ['AAA','BBB','CCC','DDD','EEE'],
 
 	startState: function () {
 		$('.stats-box').empty();
@@ -29,25 +30,23 @@ var game = {
 		game.pickQuestion();
 	},
 
-	startGame: function () {
-		$('.timer-box').hide();
-	},
-
 	pickQuestion: function () {
 		if (!game.reset) {
-
 			$('.main').show();
 			$('.timer-box').show();
+			$('.question').show();
+			$('.image-box').empty();
+			$('.answers').show();
 			game.questionCount++;
 			console.log('question count:', game.questionCount);
 			game.gameOver = false;
-			$('.image-box').empty();
-			$('.answers').show();
+			
 			game.clearTimer();
 			game.setTimer();
 			$('.timer-box').show();
 			var random = Math.floor(Math.random() * 548);
 			game.current = questionArr[random];
+			console.log('for debugging: ',game.current);
 			$('.question').html('<h2 id="question">'+game.current.question+'</h2>');
 			$('.answers').html('<h3 class="answer" id="A">A: '+game.current.A+'<h3/>'+
 								'<h3 class="answer" id="B">B: '+game.current.B+'<h3/>'+
@@ -58,14 +57,13 @@ var game = {
 	},
 
 	setTimer: function () {
-		game.timer = 31;
+		game.timer = 30;
 		$('.timer').show();
 		game.interval = setInterval(function () {
 			game.timer--;
 			console.log(game.timer);
 			$('.timer').html(game.timer);
 			if (game.timer<=0) {
-
 				game.getGiph();
 				game.showCorrect("Time's up!  ");
 				$('.answers').empty();
@@ -79,7 +77,6 @@ var game = {
 				}, 1000*6);
 			}
 		}, 1000)
-
 	},
 
 	clearTimer: function () {
@@ -114,60 +111,77 @@ var game = {
 		$('.question').html('<h2>'+value+'The correct answer is "'+game.current[game.current.answer]+'"</h2>');
 	},
 
-	getGiph: function () {
-
-	 	queryURL = "https://api.giphy.com/v1/gifs/search?q=" + game.current[game.current.answer] +"&api_key=vM9vIaMqN7X9I2ar4hHiG2SD8bYv5Zgm&limit=20"
+	getGiph: function () { 
+		var tag = game.current[game.current.answer];
+		if (Number.isInteger(tag)){
+			tag = tag.toString();
+		}
+		tag = tag.split(" ").join("+");
+		console.log('tag is : ', tag);
+		var queryURL = "https://api.giphy.com/v1/gifs/random"
+		queryURL += '?' + $.param({
+		  'api_key': "vM9vIaMqN7X9I2ar4hHiG2SD8bYv5Zgm",
+		  'tag': tag,
+		  'rating': 'g'
+		});
+		console.log('query: ', queryURL);
 	 	$.ajax({
 	      url: queryURL,
 	      method: 'GET'
 	    }).done(function(response) {
-	    	var random = Math.floor(Math.random()*response.data.length);
-	    	 $('.image-box').html("<img class='img-responsive' src=" + response.data[random].images.downsized.url + ">") 
+	    	var src = response.data.fixed_height_downsampled_url;
+	    	 $('.image-box').html("<img class='text-center' src=" + src + ">") 
 	    });
-
  	},
 
  	gameEnd: function () {
-
  		if (game.questionCount>=10) {
  			game.reset = true;
+ 			setTimeout(function () {
+ 				$('.image-box').empty();
+ 				$('.timer-box').hide();
+	 			$('.question').hide();
+	 			$('.stats-box').html('<h2>Your score is: '+game.score+'</h2>'+
+					'<h2>Number of correct answers: '+game.correct+'</h2>'+
+					'<h2>Number of incorrect answers: '+game.inCorrect+'</h2>'+
+					'<h2>Number of times that time ran out: '+game.timeUp+'</h2>'+
+					'<button class="start btn btn-warning">Play Again?</button>');
+ 			}, 1000*6);
  			var correctScore = game.correct*10;
  			var incorrectScore = game.inCorrect*10;
  			var timeUpScore = game.timeUp*5;
  			var timeScore = game.timeRemaining*2;
  			game.score = correctScore + timeScore - incorrectScore - timeUpScore; 
- 			$('.stats-box').html('<h2>Your score is: '+game.score+'</h2>'+
- 								 '<h2>Number of correct answers: '+game.correct+'</h2>'+
-				 				 '<h2>Number of incorrect answers: '+game.inCorrect+'</h2>'+
-				 				 '<h2>Number of times that time ran out: '+game.timeUp+'</h2>'+
-				 				 '<button class="start">Would you like to play again?</button>');
-
- 		}
- 		for (var i = 0; i< game.scoreArr.length; i++){
- 			if (game.score > game.scoreArr[i]) {
- 				
- 			}
- 		}
+ 			game.writeScore();
+ 		}	
  	},
 
  	writeScore: function (score) {
- 		game.scoreArr.push(score);
- 		game.scoreArr.sort();
- 		if (game.scoreArr.length>10) {
- 			game.scoreArr.pop();
+		for (var i = 0; i < game.scoreArr.length; i++){
+			var index = i + 1;
+			$('.item' + index).text(game.scoreArr[i] + " ");
+			$('.name' + index).text(" " + game.nameArr[i]);
+ 			if (game.score > game.scoreArr[i]) {
+ 				var name = prompt('What are your initials?  3 letter max ').substring(0,3).toUpperCase();
+ 				game.nameArr.splice(i, 0, name);
+ 				game.scoreArr.splice(i, 0, game.score);
+ 				game.scoreArr.pop(); 
+ 				game.nameArr.pop(); 		 		
+ 				// var index = i + 1;
+				$('.item' + index).text(game.scoreArr[i] + " ");
+				$('.name' + index).text(" " + game.nameArr[i]);
+ 				return;			
+ 			}
  		}
- 		for (var i = 0; i<game.scoreArr.length; i++) {
- 			$('.score-box').append(game.scoreArr[i]);
- 		}
- 		
+ 		game.score = 0;
  	}
-
-
 }//end of game object
 
 //event listeners
 $(document).on('click', '.start', function () {
 	$('.start').hide();
+	$('.score-box').show();
+	game.writeScore();
 	game.pickQuestion();
 	if (game.reset) {
 		game.startState();
@@ -181,6 +195,5 @@ $(document).on('click', '.answer', function () {
 		game.gameEnd();
 	}
 });
-
 
 });//end of document ready function
